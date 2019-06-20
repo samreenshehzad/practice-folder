db3fbf18235988bde2d1c752f32bcca0d08d9a08
Samreen Shehzad <49517251+samreenshehzad@users.noreply.github.com>
2019-06-20 18:01:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let car= "electic";
+if(car=="electric"){
+    alert("THIS IS MADE BY TESLA COMPANY")
+}
+let highscore=10;
+let myscore=20;
+if (highscore>myscore){
+    console.log("try again");
+}
+else{
+    console.log("congratulation");
+}
+
+let numb=10;
+if(numb%2 ==1){
+    console.log("This is ODD");
+}
